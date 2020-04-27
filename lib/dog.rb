32f3c67248c10e71ba20c760453ba4067d3eeb45
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-04-27 13:31:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog 
   def name=(dogs_name)
-    &my_dogs_name = dogs_name 
+    @my_dogs_name = dogs_name 
   end 
    def name 
-    my_dogs_name 
+    @my_dogs_name 
   end 
   
   def bark
